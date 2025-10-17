fcad009fa1b2fe7ddeb0e8564befdabe5ad06d79
Pikimell <94864615+Pikimell@users.noreply.github.com>
2025-10-17 13:24:41 +0000

    Deploying to aws-pages from @ Pikimell/english-teacher-platform@8565120e78fbcfc4f47dc6fe74dd13aad7e1aedd 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import"./assets/main-DFnbEuqS.js";import"./assets/catalog-BAgz-x48.js";import"./assets/custom-lessons-store-BKMVZjuV.js";import"./assets/custom-lesson-presets-CspSQTHi.js";import"./assets/vendor-CWxt7QI6.js";const o=document.querySelector("#g_id_onload");o.dataset.login_uri=window.location.origin;
+import"./assets/main-DFnbEuqS.js";import"./assets/catalog-B2zafu3y.js";import"./assets/custom-lessons-store-BKMVZjuV.js";import"./assets/custom-lesson-presets-CTCipuxz.js";import"./assets/vendor-CWxt7QI6.js";const o=document.querySelector("#g_id_onload");o.dataset.login_uri=window.location.origin;
 //# sourceMappingURL=index.js.map
