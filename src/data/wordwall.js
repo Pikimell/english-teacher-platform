1d9cce1da75388e83dc9672cee5cc2376342e016
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-10-22 12:33:08 +0300

    update

--- src/data/wordwall.js
+++ src/data/wordwall.js
@@ -5,12 +5,19 @@ const DEFAULT_FONT_STACK_ID = 0;
 export const lessonWordwalls = {
   'custom-no1-mggxs7zo': [
     {
-      id: 'db94624e1c8042ce98760f467377e1ae',
+      id: 'abf91e0d2ea04f8cabb896481369bcdf',
       themeId: '42',
       templateId: '70',
       fontStackId: DEFAULT_FONT_STACK_ID,
-      title: 'Wordwall вправа для особових займенників',
+      title: 'Wordwall вправа',
     },
+    // {
+    //   id: 'db94624e1c8042ce98760f467377e1ae',
+    //   themeId: '42',
+    //   templateId: '70',
+    //   fontStackId: DEFAULT_FONT_STACK_ID,
+    //   title: 'Wordwall вправа для особових займенників',
+    // },
   ],
   'custom-no2-4db25661': [
     {
@@ -42,20 +49,61 @@ export const lessonWordwalls = {
       url: 'https://wordwall.net/embed/110f5ab8bba8415eadefd09fb52c4e87?themeId=65&templateId=30&fontStackId=12',
     },
   ],
-  // 'custom-no5-a3c6ccb2': [],
-  // 'custom-no4-c6f6abec': [],
-  // 'custom-no4-c6f6abec': [],
-  // 'custom-no4-c6f6abec': [],
-  // 'custom-no4-c6f6abec': [],
-  // 'custom-no4-c6f6abec': [],
-  // 'custom-no4-c6f6abec': [],
-  // 'custom-no4-c6f6abec': [],
-  // 'custom-no4-c6f6abec': [],
-  // 'custom-no4-c6f6abec': [],
-  // 'custom-no4-c6f6abec': [],
-  // 'custom-no4-c6f6abec': [],
-  // 'custom-no4-c6f6abec': [],
-  // 'custom-no4-c6f6abec': [],
+  'custom-no5-a3c6ccb2': [
+    {
+      id: 'cd69f6a25d164b098c91cb9f50c307ee',
+      themeId: '65',
+      templateId: '30',
+      fontStackId: '12',
+      title: 'Wordwall вправа для особових займенників',
+      url: 'https://wordwall.net/embed/cd69f6a25d164b098c91cb9f50c307ee?themeId=65&templateId=30&fontStackId=0',
+    },
+  ],
+  'custom-no6-da70b690': [],
+  'custom-no7-43000f0c': [],
+  'custom-no8-846a8d9e': [],
+  'custom-no9-cf6e6bee': [],
+  'custom-no10-28500f64': [],
+  'custom-no11-d8d86e31': [],
+  'custom-no12-93e53fe8': [],
+  'custom-no13-f4bcb910': [],
+  'custom-no14-40fdaa86': [],
+  'custom-no15-ef51a27e': [],
+  'custom-no16-e3b3d783': [],
+  'custom-no17-6337b1c4': [],
+  'custom-no18-b8435532': [],
+  'custom-no19-8d87b15e': [],
+  'custom-no20-06a86866': [],
+  'custom-no21-670eb82c': [],
+  'custom-no22-2073a55b': [],
+  'custom-no23-21e7300b': [],
+  'custom-no24-29c54cf7': [],
+  'custom-no25-796a6730': [],
+  'custom-no26-9b5f1ffc': [],
+  'custom-no27-6487572a': [],
+  'custom-no28-7f7a2815': [],
+  'custom-no29-9710e19a': [],
+  'custom-no30-b72e7840': [],
+  'custom-no31-2b6f7bda': [],
+  'custom-no32-deb561b3': [],
+  'custom-no33-9a2f527a': [],
+  'custom-no34-5b2c8e22': [],
+  'custom-no35-a6a8ac33': [],
+  'custom-no36-6a082f08': [],
+  'custom-no37-faa9b7b8': [],
+  'custom-no38-5206027c': [],
+  'custom-no39-6b35cfd3': [],
+  'custom-no40-4927837a': [],
+  'custom-no41-7ba1db93': [],
+  'custom-no42-29b5bb10': [],
+  'custom-no43-7ecfedc0': [],
+  'custom-no44-0e8b8ebd': [],
+  'custom-no45-310fbeb4': [],
+  'custom-no46-ccb1de5a': [],
+  'custom-no47-d6148758': [],
+  'custom-no48-3e5720c9': [],
+  'custom-no49-f759cafd': [],
+  'custom-no50-23930ae8': [],
 };
 
 function buildBaseUrl(config) {
@@ -111,3 +159,9 @@ export function getWordwallConfigsForLesson(lessonId) {
     .map(entry => ({ ...entry }))
     .filter(entry => Boolean(buildWordwallUrl(entry)));
 }
+
+/* 
+<iframe style="max-width:100%" src="https://wordwall.net/embed/cd69f6a25d164b098c91cb9f50c307ee?themeId=65&templateId=30&fontStackId=0" width="500" height="380" frameborder="0" allowfullscreen></iframe>
+<iframe style="max-width:100%" src="https://wordwall.net/embed/cd69f6a25d164b098c91cb9f50c307ee?themeId=65&templateId=30&fontStackId=0" width="500" height="380" frameborder="0" allowfullscreen></iframe>
+<iframe style="max-width:100%" src="https://wordwall.net/embed/4ccd2571348045fa98270675605b2dd2?themeId=65&templateId=30&fontStackId=0" width="500" height="380" frameborder="0" allowfullscreen></iframe>
+*/
